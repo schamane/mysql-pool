1f393ce3ecc616adb5316fcec08fd04f38f1e5da
Dima <dima_16rus@mail.ru>
2012-07-08 14:38:56 +0400

    Добавлен автономный возврат соединения в пул

--- index.js
+++ index.js
@@ -1,41 +1,52 @@
 module.exports = function (properties) {
+    var self = this;
     var mysql = require('mysql');
-    var poolSize = 1;
-    var parameters = {};
     var connectionPool = [];
     var mainQueue = [];
-
+   
     (function () {
-        for (var key in properties) {
-            switch (key) {
-                case 'poolSize' : {
-                    poolSize = properties[key]; 
-                    break;
-                };
-                default : {
-                    parameters[key] = properties[key];
-                };
-            };
-        };
-        for (var x = 0; x < poolSize; x ++) {
-            connectionPool.push(mysql.createConnection(parameters));
-        };
+       var poolSize = properties.poolSize;
+       delete properties.poolSize;
+       for (var x = 0; x < poolSize; x += 1) {
+           connectionPool.push({ 
+               connection : mysql.createConnection(properties),
+               cid : x
+           });
+       };
     })();
 
+    var resumeConnection = function(connection) {
+        delete connection.query;  
+        connectionPool.push(connection);
+        if (mainQueue.length) { 
+            process.nextTick(function () {
+                self.getConnection(mainQueue.shift());
+            });
+        };
+    };
+
     this.getConnection = function(callback) {
-        if (connectionPool.length) {
-            callback(connectionPool.pop());
-        } else { 
+        var connection;
+        if (connectionPool.length) { 
+            connection = connectionPool.pop(); 
+            connection.query = function (sql, params, callback) { 
+                if (typeof params == "function") {
+                    callback = params;
+                    params = undefined;
+                };
+                this.connection.query(sql, params || [], function() { 
+                    if (arguments[0] !== null) 
+                        resumeConnection(this);
+                    if (this.connection._protocol._queue.length == 1) 
+                        resumeConnection(this);   
+                    if (callback) 
+                        callback.apply(this.connection, arguments); 
+                }.bind(this));
+            };
+            callback(connection);
+        } else {
             mainQueue.push(callback);
         };
     };
-
-    this.resume = function(connection) {
-        connectionPool.push(connection);
-        if (mainQueue.length) {
-            process.nextTick(function (
-                this.getConnection(mainQueue.shift());
-            }.bind(this));
-        };
-    };
 };
+
